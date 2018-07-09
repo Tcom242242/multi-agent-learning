0d704ad4dfe0f7229884a74964fca97fe1ad1a4e
Takuya Okano <okano565656@gmail.com>
2018-07-09 21:05:50 +0900

    minor change

--- run_phc.py
+++ run_phc.py
@@ -6,7 +6,6 @@ from tqdm import tqdm
 import numpy as np
 import matplotlib.pyplot as plt
 import ipdb
-import math
 from agents.phc_agent import PHCAgent
 from agents.policy import NormalPolicy
 from games.simple_game import SimpleGame
@@ -20,10 +19,12 @@ if __name__ == '__main__':
         agent = PHCAgent(alpha=0.1, policy=policy, action_list=np.arange(2))  # agentの設定
         agents.append(agent)
 
-    game = SimpleGame(nb_steps=400000, agents=agents)
+    game = SimpleGame(nb_steps=100000, agents=agents)
     game.run()
     for idx, agent in enumerate(agents):
         print("agent{}s average reward:{}".format(idx, np.mean(agent.rewards)))
     plt.plot(np.arange(len(agents[0].pi_history)),agents[0].pi_history)
+    plt.ylabel("Probability of selecting Heads")
+    plt.xlabel("Step")
     plt.ylim(0, 1)
     plt.show()
